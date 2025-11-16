4832e2d73adfdd53354afbcebbbf82d0ce3d4ce6
Cuga77 <bogatov2021@mail.ru>
2025-11-16 17:26:00 +0300

    Индекс по status_id бесполезен, так как статусов всего два (низкая селективность). Заменил его на составной индекс (Автор + Статус)

--- migrations/000004_create_pull_requests.up.sql
+++ migrations/000004_create_pull_requests.up.sql
@@ -3,11 +3,9 @@ CREATE TABLE pull_requests (
     pull_request_name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     merged_at TIMESTAMP WITH TIME ZONE,
-
     author_id VARCHAR(255) NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
-
     status_id SMALLINT NOT NULL DEFAULT 1 REFERENCES pr_statuses(id) ON DELETE RESTRICT
 );
 
 CREATE INDEX idx_pull_requests_author_id ON pull_requests(author_id);
-CREATE INDEX idx_pull_requests_status_id ON pull_requests(status_id);
+CREATE INDEX idx_pull_requests_author_status ON pull_requests(author_id, status_id);
